00511d4ed9f42d005aa9c8004cb660a05efedd87
Hacker_Spongebob <65222372+Septi-21@users.noreply.github.com>
2020-07-01 13:53:05 +0700

    Update Bahan.sh

--- Bahan.sh
+++ Bahan.sh
@@ -1,7 +1,7 @@
 #bin/bash
 clear
 echo
-echo "  ⚠️ ℙ𝕣𝕠𝕤𝕖𝕤 𝕄𝕖𝕟𝕘𝕚𝕟𝕤𝕥𝕒𝕝𝕝 𝔹𝕒𝕙𝕒𝕟 𝕊𝕒𝕓𝕒𝕣 𝕐𝕒 𝕄𝕒𝕞𝕒𝕟𝕘 ⚠️  "
+echo "  ⚠️ ℙ𝕣𝕠𝕤𝕖𝕤 𝕄𝕖𝕟𝕘𝕚𝕟𝕤𝕥𝕒𝕝𝕝 𝔹𝕒𝕙𝕒𝕟² 𝕊𝕒𝕓𝕒𝕣 𝕐𝕒 𝕄𝕒𝕞𝕒𝕟𝕘 ⚠️  "
 echo
 cd $HOME
 pkg update && pkg upgrade
